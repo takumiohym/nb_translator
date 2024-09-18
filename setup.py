0d4beec067bf2268ffd452385bf535911515cbae
Takumi Ohyama <takumiohym@google.com>
2024-09-18 14:15:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="nb_translator",
     version="0.1.0",
     author='Takumi Ohyama',
-    author_email='takumi10di@gmail.com',
+    author_email='takumiohym@google.com',
     url='https://github.com/takumiohym/nb_translator',
     packages=find_packages(),
     install_requires=["fire", "google-cloud-translate"],
